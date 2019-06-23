8df524ddd05a290e57a3cb64731465d8e521bead
andie5 <andieasa@googlemail.com>
2019-06-23 23:55:21 +0100

    Grocery list using internal state

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -5,13 +5,57 @@ class GroceryList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            groceries : []
+            groceries : [],
+            item: ''
         }
+    };
+
+    addToList = (event) => {
+        event.preventDefault();
+        this.setState({groceries: [...this.state.groceries, this.state.item]})
+        console.log("groceries: ", this.state.groceries)
     }
+
+    handleChange = (event) => {
+        this.setState({item: event.target.value})
+        console.log("event: ", event.target.value)
+    }
+
     render(){
-        return(
-            <button>Add to grocery list</button>
-        );
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <form onSubmit={this.addToList}>
+                            <label>Enter grocery item</label><br />
+                            <input type="text"
+                                placeholder="Enter grocery item"
+                                name="groceryitem"
+                                value={this.state.item}
+                                onChange={this.handleChange}
+                            >
+                            </input><br />
+                            <p></p>
+                            <button>Add to List</button>
+                        </form>
+                        {/* <button>Add to grocery list</button><br /> */}
+                    </div>
+                    <div className="col">
+                        <h3>Grocery List</h3>
+                        <ul>
+                            {this.state.groceries.map((item) =>
+                                <li>{item}</li>
+                                )
+                            }
+        
+                        </ul>
+                          
+                    </div>
+                </div>
+
+            </div>
+        )
+        
     }
 }
 
