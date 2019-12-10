a553488f46d45af06497192ccfa6996a9277fedc
andie5 <andieasa@googlemail.com>
2019-12-10 22:28:03 +0000

    cleanup of commented code

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,174 +1,176 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import {addItemToGroceryList, removeItemToGroceryList, updateDescriptionOfItem } from './actions'
+import React from "react";
+import { connect } from "react-redux";
+import {
+  addItemToGroceryList,
+  removeItemToGroceryList,
+  updateDescriptionOfItem
+} from "./actions";
 
 class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idToUpdate: 0,
+      id: 0
+    };
+    this.groceryitem = React.createRef();
+    this.description = React.createRef();
+    this.updatedDescription = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            // groceries : [],
-            // item: '', 
-            // updatedDescription: '',
-            idToUpdate: 0,
-            id: 0
-        }
-        this.groceryitem = React.createRef();
-        this.description = React.createRef();
-        this.updatedDescription = React.createRef();
+  /**
+   * Action creator to add a item to the grocery list
+   * @param {Event} event - The event object from the user interaction on the interface
+   */
+  addToList = event => {
+    // Prevent the page reloading each time this function is called
+    event.preventDefault();
+
+    // If we were storing the list in local this.state, do the following
+    // this.setState({groceries: [...this.state.groceries, this.state.item]})
+
+    // Temporary until the id is added to redux
+    this.setState({ id: this.state.id + 1 });
+
+    // Create an object from each shopping list item
+    // Create utility function for the creating new item
+    const newItem = {
+      id: this.state.id,
+      name: this.groceryitem.current.value,
+      description: this.description.current.value,
+      purchased: true
     };
 
+    // Redux state
+    this.props.addItemToGroceryList(newItem);
+  };
 
-    addToList = (event) => {
-        // Prevent the page reloading each time this function is called
-        event.preventDefault();
+  /**
+   * Action creator to remove an item to the grocery list
+   * @param {Event} event - The event object from the user interaction on the interface
+   */
+  removeItem = event => {
+    // Prevent the page reloading each time this function is called
+    //  event.preventDefault();
+    this.props.removeItemToGroceryList(event.target.value);
+  };
 
-        // If we were storing the list in local this.state, do the following
-        // this.setState({groceries: [...this.state.groceries, this.state.item]})
+  /**
+   * Action creator to update an item in the grocery list
+   * @param {Event} event - The event object from the user interaction on the interface
+   */
+  updateItem = event => {
+    // Prevent the page reloading each time this function is called
+    event.preventDefault();
 
-        // Temporary until the id is added to redux
-        this.setState({id: this.state.id + 1})
+    // this.props.updateDescriptionOfItem(event.target.value, this.state.idToUpdate)
+    this.props.updateDescriptionOfItem(
+      this.state.idToUpdate,
+      this.updatedDescription.current.value
+    );
+  };
 
-        // Create an object from each shopping list item
-        // Create utility function for the creating new item
-        const newItem = {
-            id: this.state.id,
-            name: this.groceryitem.current.value,
-            description: this.description.current.value,
-            // category: this.state.category,
-            purchased: true
-        }
-        console.log("this.groceryitem: ", this.groceryitem.current.value)
-        console.log("newItem: ", newItem)
+  /**
+   * Update the state each time when the description is updated
+   * @param {Event} event - The event object from the user interaction on the interface
+   */
+  handleDescriptionChange = event => {
+    this.setState({ idToUpdate: event.target.value });
+  };
 
-        // Redux state
-        this.props.addItemToGroceryList(newItem)
-    }
-
-    removeItem = (event) => {
-         // Prevent the page reloading each time this function is called
-        //  event.preventDefault();
-         this.props.removeItemToGroceryList(event.target.value)
-    }
-
-    updateItem = (event) => {
-        // Prevent the page reloading each time this function is called
-        event.preventDefault();
-
-        console.log("event.target.value ", event.target.value)
-        console.log("this.updatedDescription.current.value ", this.updatedDescription.current.value)
-        console.log("this.updatedDescription ", this.updatedDescription)
-        
-
-        // this.props.updateDescriptionOfItem(event.target.value, this.state.idToUpdate)
-        this.props.updateDescriptionOfItem(this.state.idToUpdate, this.updatedDescription.current.value)
-        console.log("updated desc: ", this.props.groceries)
-   }
-
-   handleDescriptionChange = (event) => {
-    this.setState({idToUpdate: event.target.value})
-    console.log("this.state.idToUpdate: ", this.state.idToUpdate)
-    }
-//    handleDropdownChange = (event) => {
-//     this.setState({category: event.target.value})
-//    }
-
-    render(){
-console.log("groceries in main: ", this.props.groceries)
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <form className="container" onSubmit={this.addToList}>
-                            <h1>
-                                Add A Grocery
-                            </h1>
-                            <input type="text"
-                                placeholder="Enter grocery item"
-                                name="groceryitem"
-                                ref={this.groceryitem}
-                            >
-                            </input><br />
-                            <input type="text"
-                                placeholder="Enter description"
-                                name="description"
-                                ref={this.description}
-                            >
-                            </input>
-                            <br />
-                            <select name ="category" 
-                             onChange={this.handleDropdownChange}
-                            >
-                                <option value = "Food & Drink" defaultValue>Food & Drink</option>
-                                <option value = "Clothing">Clothing</option>
-                                <option value = "Toiletories">Toiletories</option>
-                            </select>
-                            <p></p>
-                            <button>Add to List</button>
-                        </form>
-                    </div>
-                    <div className="col">
-                        <h3>Grocery List</h3>
-                        <ul>
-                            {this.props.groceries.map((item) => {
-
-                                   return <li key={item.id}>{item.name}</li>
-                                    
-                                })
-                            }
-                        </ul>
-                    </div>
-                    <div className="col">
-                    <h3>Remove an item from grocery list</h3>
-                        <select name ="groceryList"
-                            onChange={this.removeItem} 
-                            >
-                            {this.props.groceries.map((item) => {
-
-                                return <option key={item.id} value={item.name}>{item.name}</option>
-                             })
-                            }
-                        </select>
-                        {/* Use onclick if I want to use local state or use redux to store the text that the user is entering */}
-                        {/* <button onClick={this.removeItem}>Delete item</button> */}
-                    </div>
-                    {/* Using select have to use onchange rather than onSubmit with uncontrolled components */}
-                    <div className="col">
-                        <h3>Update an item from grocery list</h3>
-                        <form className="container" 
-                            onSubmit={this.updateItem}
-                        >
-                            <select name ="groceryList" onChange={this.handleDescriptionChange}>
-                                {
-                                    this.props.groceries.map((item) => {
-                                        return <option key={item.id} value={item.id}>{item.name}</option>
-                                    })
-                                }
-                            </select>
-                            <input type="text"
-                                    placeholder="Enter new description"
-                                    name="description"
-                                    ref={this.updatedDescription}
-                                >
-                            </input>
-                            <button>Update description</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form className="container" onSubmit={this.addToList}>
+              <h1>Add A Grocery</h1>
+              <input
+                type="text"
+                placeholder="Enter grocery item"
+                name="groceryitem"
+                ref={this.groceryitem}
+              ></input>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter description"
+                name="description"
+                ref={this.description}
+              ></input>
+              <br />
+              <select name="category" onChange={this.handleDropdownChange}>
+                <option value="Food & Drink" defaultValue>
+                  Food & Drink
+                </option>
+                <option value="Clothing">Clothing</option>
+                <option value="Toiletories">Toiletories</option>
+              </select>
+              <p></p>
+              <button>Add to List</button>
+            </form>
+          </div>
+          <div className="col">
+            <h3>Grocery List</h3>
+            <ul>
+              {this.props.groceries.map(item => {
+                return <li key={item.id}>{item.name}</li>;
+              })}
+            </ul>
+          </div>
+          <div className="col">
+            <h3>Remove an item from grocery list</h3>
+            <select name="groceryList" onChange={this.removeItem}>
+              {this.props.groceries.map(item => {
+                return (
+                  <option key={item.id} value={item.name}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            {/* Use onclick if I want to use local state or use redux to store the text that the user is entering */}
+            {/* <button onClick={this.removeItem}>Delete item</button> */}
+          </div>
+          {/* Using select have to use onchange rather than onSubmit with uncontrolled components */}
+          <div className="col">
+            <h3>Update an item from grocery list</h3>
+            <form className="container" onSubmit={this.updateItem}>
+              <select
+                name="groceryList"
+                onChange={this.handleDescriptionChange}
+              >
+                {this.props.groceries.map(item => {
+                  return (
+                    <option key={item.id} value={item.id}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+              </select>
+              <input
+                type="text"
+                placeholder="Enter new description"
+                name="description"
+                ref={this.updatedDescription}
+              ></input>
+              <button>Update description</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return { groceries: state.groceries }; // this.props.grocerieslist
-}
+const mapStateToProps = state => {
+  return { groceries: state.groceries }; // this.props.grocerieslist
+};
 
-const mapDispatchToProps = { addItemToGroceryList, removeItemToGroceryList, updateDescriptionOfItem }
+const mapDispatchToProps = {
+  addItemToGroceryList,
+  removeItemToGroceryList,
+  updateDescriptionOfItem
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(GroceryList)
-
-// export default GroceryList;
+export default connect(mapStateToProps, mapDispatchToProps)(GroceryList);
--- src/reducers.js
+++ src/reducers.js
@@ -1,77 +1,38 @@
-
-import { combineReducers } from 'redux';
-import {ADD_ITEM_TO_LIST, REMOVE_ITEM_FROM_LIST, UPDATE_ITEM_IN_LIST} from './actionTypes'
+import { combineReducers } from "redux";
+import {
+  ADD_ITEM_TO_LIST,
+  REMOVE_ITEM_FROM_LIST,
+  UPDATE_ITEM_IN_LIST
+} from "./actionTypes";
 
 // Reducer to as a single text item to an array or remove it
-
-// why d we need to initialise state each time in the parameters
 const groceries = (state = [], action) => {
-    switch(action.type){
-        case ADD_ITEM_TO_LIST:
-            return [...state , action.payload];
-        case REMOVE_ITEM_FROM_LIST:
-            console.log("action.payload: ", action.payload)
-            
-            // Find the whole item to delete in the collection
-            const groceryItemObj = state.find(item => item.name === action.payload);
+  switch (action.type) {
+    case ADD_ITEM_TO_LIST:
+      return [...state, action.payload];
+    case REMOVE_ITEM_FROM_LIST:
+      // Find the whole item to delete in the collection
+      const groceryItemObj = state.find(item => item.name === action.payload);
 
-            // Delete the item using the id
-            // wjy was passing the id not enough and the whole object from the selection had to be retrieved
-            // return state.filter((item) => item.id!==action.id)
-            return state.filter((item) => item.id!==groceryItemObj.id)
+      // Delete the item using the id
+      // wjy was passing the id not enough and the whole object from the selection had to be retrieved
+      // return state.filter((item) => item.id!==action.id)
+      return state.filter(item => item.id !== groceryItemObj.id);
 
-        case UPDATE_ITEM_IN_LIST:
-
-            console.log("action.payload: ", action.payload)
-            // Find the whole item to delete in the collection
-            // let groceryItemToUpdate = state.find(item => item.name === action);
-
-            // console.log("groceryItemToUpdate: ", groceryItemToUpdate)
-        
-                return state.map(item => {
-                    console.log("item.id is: ", item.id, " and action.id is: ", action.payload.id)
-                    if (item.id === parseInt(action.payload.id)){
-                        console.log("found match: ")
-                        console.log("item to update: " ,{...item, description: action.payload.newDescription})
-                        return {...item, description: action.payload.newDescription}
-                    }
-                    else {
-                        return item
-                    }
-                })
-        default:
-            return state;
-    }
+    case UPDATE_ITEM_IN_LIST:
+      // Find the whole item to delete in the collection
+      return state.map(item => {
+        if (item.id === parseInt(action.payload.id)) {
+          return { ...item, description: action.payload.newDescription };
+        } else {
+          return item;
+        }
+      });
+    default:
+      return state;
+  }
 };
 
-
-// const peopleInitialState = [
-//     {
-//         id: 123,
-//         name: 'Joe',
-//         age: 33,
-//         hobbies: ['painting','dancing']
-//     },
-// ]
-// const peopleReducer = (state = peopleInitialState, action) => {
-//     switch(action.type){
-//         case ADD_HOBBY: {
-//             // expected: action.id
-//             // expected: action.hobby -- eg. 'walking'
-//             return state.map(person => {
-//                 if(person.id !== action.id) return person; // <-- returned by reference, not cloned, we don't need to clone it if we don't want to change it
-//                 return {...person,
-//                     hobbies: [...person.hobbies, action.hobby]
-//                 }
-//             })
-//         }
-        
-//         default:
-//             return state;
-//     }
-// }
-
 export const reducers = combineReducers({
-  groceries,
-}); 
-
+  groceries
+});
